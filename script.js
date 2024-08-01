491c0658e7a8f07898c2c4bcba0afea4ef30418d
hemanthbommana <117904125+hemanthbommana@users.noreply.github.com>
2024-08-01 16:40:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+document.addEventListener('DOMContentLoaded',UpdateTimer());
+setInterval(UpdateTimer, 1000);
+	function UpdateTimer(){
+		let ans = document.getElementById("timer") ;
+		let now = new Date();
+		let hours = now.getHours();
+		    hours = hours % 12;
+            hours = hours ? hours : 12;
+		let minutes = now.getMinutes();
+		let seconds = now.getSeconds();
+		let period = hours >= 12 ? 'PM' : 'AM';
+		ans.innerHTML = (now.getDate()+"/"+now.getMonth()+"/"+now.getFullYear()+", "+hours+":"+minutes+":"+seconds+" "+period);
+		
+	}
